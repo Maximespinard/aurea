1a067d680ae48295dda70d846620d74a0e6e1a46
mspinard <mspinard@vulog.com>
2025-07-06 11:45:24 +0200

    (feat) fonctional navbar completed

--- client/src/components/layout/public/Navbar.tsx
+++ client/src/components/layout/public/Navbar.tsx
@@ -1,4 +1,28 @@
+import { Button } from '@/components/ui/button';
+import { NavLink, useLocation } from 'react-router';
+
 const Navbar = () => {
-  return <nav>Navbar</nav>;
+  const location = useLocation();
+  const hideAuthLink = ['/signin', '/register'].includes(location.pathname);
+
+  return (
+    <header className=" border-b bg-background px-4 md:px-8 h-[80px]">
+      <div className="mx-auto h-full flex max-w-7xl items-center justify-between px-4 py-4 sm:px-6 lg:px-8">
+        <NavLink
+          to="/"
+          className="text-2xl font-semibold tracking-tight text-primary"
+        >
+          Auréa
+        </NavLink>
+        <nav className="flex items-center gap-8">
+          {!hideAuthLink && (
+            <NavLink to="/signin">
+              <Button>Sign In</Button>
+            </NavLink>
+          )}
+        </nav>
+      </div>
+    </header>
+  );
 };
 export default Navbar;
