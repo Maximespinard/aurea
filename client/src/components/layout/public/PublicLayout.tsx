fa8344b1ff42591b57f9bcda38d5dfbbb718a17b
mspinard <mspinard@vulog.com>
2025-07-06 11:02:50 +0200

    (style) Container component created

--- client/src/components/layout/public/PublicLayout.tsx
+++ client/src/components/layout/public/PublicLayout.tsx
@@ -1,13 +1,16 @@
 import { Outlet } from 'react-router';
 import Navbar from '@/components/layout/public/Navbar';
 import Footer from '@/components/layout/public/Footer';
+import Container from '@/components/shared/Container';
 
 export default function PublicLayout() {
   return (
     <>
       <Navbar />
       <main>
-        <Outlet />
+        <Container>
+          <Outlet />
+        </Container>
       </main>
       <Footer />
     </>
