a9071e6afa3b0fc5081ceea942716dd4f0bbd429
mspinard <mspinard@vulog.com>
2025-07-09 10:11:27 +0200

    feat: setup toast notifications with dark theme styling

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { Toaster } from 'react-hot-toast';
 import './index.css';
 import App from './App.tsx';
 
@@ -19,6 +20,16 @@ createRoot(document.getElementById('root')!).render(
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
         <App />
+        <Toaster
+          position="top-right"
+          toastOptions={{
+            duration: 4000,
+            style: {
+              background: '#363636',
+              color: '#fff',
+            },
+          }}
+        />
       </BrowserRouter>
     </QueryClientProvider>
   </StrictMode>
